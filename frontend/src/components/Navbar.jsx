7a51994f963c411f008f3aaed5fbc41b0586dcf8
mdMERAJ77 <mmd200975@gmail.com>
2026-01-29 18:09:57 +0530

    search functionality added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ import {
 } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { API } from "../utils/api";
-import SearchBar from '../Search/SearchBar';
+import SearchBar from '../components/Search/SearchBar';
 // ========== COMPONENTS DEFINED OUTSIDE RENDER ==========
 
 const NavLink = memo(({ to, iconType, children, isActive, onClick }) => {
